42b963e5239d58536c8a1b729584b2e743ddcc16
Liams-theCreator <mmeellaallii1@gmail.com>
2026-06-07 21:14:00 +0100

    Create header file & implement OCF

--- CPP-Module-05/ex00/Bureaucrat.cpp
+++ CPP-Module-05/ex00/Bureaucrat.cpp
@@ -1,1 +1,33 @@
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() {}
+Bureaucrat::Bureaucrat(const Bureaucrat &other) 
+{
+    *this = other;
+}
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat &other)
+{
+    if (this != &other)
+        *this = other;
+    return *this;
+}
+Bureaucrat::~Bureaucrat() {}
+
+std::string Bureaucrat::getName()
+{
+    return _name;
+}
+unsigned int Bureaucrat::getGrade()
+{
+    return _grade;
+}
+
+void incGrade()
+{
+    _grade++;
+}
+
+void decGrade()
+{
+    _grade--;
+}
--- CPP-Module-05/ex00/Bureaucrat.hpp
+++ CPP-Module-05/ex00/Bureaucrat.hpp
@@ -1,6 +1,25 @@
 #ifndef BUREAUCRAT_H
 #define BUREAUCRAT_H
 
+#include <iostream>
+#include <string>
 
+class Bureaucrat 
+{
+    private:
+        const std::string _name;
+        unsigned int _grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(const Bureaucrat &other);
+        Bureaucrat& operator=(const Bureaucrat &other);
+        ~Bureaucrat();
+
+        std::string getName();
+        unsigned int getGrade();
+
+        void incGrade();
+        void decGrade();
+}
 
 #endif
