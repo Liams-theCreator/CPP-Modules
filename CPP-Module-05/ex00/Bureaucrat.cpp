0cf9c1418b067a9b8647f268484b150cc431625f
Liams-theCreator <mmeellaallii1@gmail.com>
2026-06-08 14:05:38 +0100

    testing after small changes on parametrized constructor

--- CPP-Module-05/ex00/Bureaucrat.cpp
+++ CPP-Module-05/ex00/Bureaucrat.cpp
@@ -1,7 +1,13 @@
 #include "Bureaucrat.hpp"
 
 Bureaucrat::Bureaucrat() {}
-Bureaucrat::Bureaucrat(const Bureaucrat &other): _name(other._name), _grade(other._grade) {}
+Bureaucrat::Bureaucrat(const Bureaucrat &other): _name(other._name), _grade(other._grade)
+{
+    if (_grade < 1)
+        throw GradeTooHighException();
+    else if (_grade > 150)
+        throw GradeTooLowException();
+}
 Bureaucrat::Bureaucrat(const std::string name, int grade): _name(name), _grade(grade) {}
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat &other)
