8a9f6df584ec923514bfdc5ba1c596b1565bb769
Liams-theCreator <mmeellaallii1@gmail.com>
2026-06-10 22:23:34 +0100

    Serializer

---
+++ CPP-Module-06/ex01/Data.hpp
@@ -0,0 +1,10 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+struct Data
+{
+    size_t data_size;
+    std::string data_name;
+};
+
+#endif
---
+++ CPP-Module-06/ex01/Serializer.cpp
@@ -0,0 +1,23 @@
+#include "Serializer.hpp"
+
+Serializer::Serializer() {}
+Serializer::Serializer(const Serializer &obj)
+{
+    (void)obj;
+}
+Serializer &Serializer::operator=(const Serializer &obj) 
+{
+    (void)obj;
+    return *this;
+}
+Serializer::~Serializer() {}
+
+static uintptr_t Serializer::serialize(Data* ptr)
+{
+    return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+static Data* Serializer::deserialize(uintptr_t raw)
+{
+    return (reinterpret_cast<Data*>(raw));
+}
